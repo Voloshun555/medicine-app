cac720a31c53375902a57cba82d07e0107abc761
Voloshun555 <Voloshunihor@gmail.com>
2024-03-06 23:56:55 +0200

    add header

---
+++ src/components/heder/heder.tsx
@@ -0,0 +1,18 @@
+import logo from "../../image/medicine-loga.svg";
+import s from "./heder.module.scss";
+import { CustomLink } from "../custom-nav-link.tsx/custum-link";
+import { Link } from "react-router-dom";
+
+export const Heder = () => {
+  return (
+    <header className={s.heder}>
+      <Link to="/">
+        <img src={logo} height={50} width={50} alt="logo" />
+      </Link>
+      <div>
+        <CustomLink to="/">Pharmacies</CustomLink>
+        <CustomLink to="/456">Sopping cart</CustomLink>
+      </div>
+    </header>
+  );
+};
---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,19 @@
+
+import { Suspense } from "react";
+import { Heder } from "../heder/heder"
+import s from "./layout.module.scss"
+import { Outlet } from "react-router-dom";
+
+
+export const Layout = () => {
+  return (
+    <>
+      <Heder />
+      <main>
+        <Suspense fallback="Завантаження...">
+          <Outlet />
+        </Suspense>
+      </main>
+    </>
+  );
+}
